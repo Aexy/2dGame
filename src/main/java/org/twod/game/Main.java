84ba7b7b83f379781b202a6edf8569da448348ba
aexy <k12330953@students.jku.at>
2024-08-19 22:13:21 +0200

    Handle Keys

---
+++ src/main/java/org/twod/game/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("2D Game");
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
